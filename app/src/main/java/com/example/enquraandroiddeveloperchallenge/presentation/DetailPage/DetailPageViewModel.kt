27d6975aa2b65e860b8537e2553d96df01617968
Hüseyin Özkoç <huseyin.ozko225@gmail.com>
2024-05-15 16:19:53 +0300

    Created ViewModels for all pages in the project.

---
+++ app/src/main/java/com/example/enquraandroiddeveloperchallenge/presentation/DetailPage/DetailPageViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.enquraandroiddeveloperchallenge.presentation.DetailPage
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailPageViewModel @Inject constructor() : ViewModel() {
+}
+
---
+++ app/src/main/java/com/example/enquraandroiddeveloperchallenge/presentation/HomePage/HomePageViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.enquraandroiddeveloperchallenge.presentation.HomePage
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomePageViewModel @Inject constructor() : ViewModel() {
+}
