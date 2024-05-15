483076ad2b5c399fc09aac28d870a5cb460ecac5
Hüseyin Özkoç <huseyin.ozko225@gmail.com>
2024-05-15 15:45:41 +0300

    Added Hilt into the Application, and created AndroidEntryPoint.

--- app/src/main/java/com/example/enquraandroiddeveloperchallenge/MainActivity.kt
+++ app/src/main/java/com/example/enquraandroiddeveloperchallenge/MainActivity.kt
@@ -15,7 +15,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.enquraandroiddeveloperchallenge.ui.theme.EnquraAndroidDeveloperChallengeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
