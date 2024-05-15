5f20194d5982984d3d34d7b24c9e809763408fb9
Hüseyin Özkoç <huseyin.ozko225@gmail.com>
2024-05-15 15:29:01 +0300

    Implemented SplashScreen for project.

--- app/src/main/java/com/example/enquraandroiddeveloperchallenge/MainActivity.kt
+++ app/src/main/java/com/example/enquraandroiddeveloperchallenge/MainActivity.kt
@@ -5,9 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
--- app/src/main/java/com/example/enquraandroiddeveloperchallenge/SplashPage.kt
+++ app/src/main/java/com/example/enquraandroiddeveloperchallenge/SplashPage.kt
@@ -1,9 +1,63 @@
 package com.example.enquraandroiddeveloperchallenge
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashPage(navController: NavController) {
+    var isPlaying by remember {
+        mutableStateOf(true)
+    }
 
+    var speed by remember {
+        mutableStateOf(1f)
+    }
+
+    val composition by rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.animation_splash)
+    )
+
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = LottieConstants.IterateForever,
+        isPlaying = isPlaying,
+        speed = speed,
+        restartOnPlay = false
+    )
+
+    LaunchedEffect(Unit) {
+        delay(5000)
+        navController.navigate(Screen.HomePage.route)
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Cyan),
+        contentAlignment = Alignment.Center,
+    ) {
+        LottieAnimation(
+            composition = composition,
+            progress = progress,
+            modifier = Modifier.fillMaxSize(),
+            alignment = Alignment.Center
+        )
+    }
 }
