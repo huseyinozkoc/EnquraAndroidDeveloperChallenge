cdefd9031297a97c352f9e993effa0e34269f9dd
Hüseyin Özkoç <huseyin.ozko225@gmail.com>
2024-05-15 16:41:45 +0300

    Created Retrofit DI module for project, and created BankService interface for Retrofit requests.

--- app/src/main/java/com/example/enquraandroiddeveloperchallenge/data/BankService.kt
+++ app/src/main/java/com/example/enquraandroiddeveloperchallenge/data/BankService.kt
@@ -1,4 +1,9 @@
 package com.example.enquraandroiddeveloperchallenge.data
 
+import com.example.enquraandroiddeveloperchallenge.data.model.BankData
+import retrofit2.http.GET
+
 interface BankService {
+    @GET(".")
+    suspend fun getBankDataList(): List<BankData>
 }
---
+++ app/src/main/java/com/example/enquraandroiddeveloperchallenge/di/RetrofitModule.kt
@@ -0,0 +1,22 @@
+package com.example.enquraandroiddeveloperchallenge.di
+
+import com.example.enquraandroiddeveloperchallenge.common.Constants.BASE_URL
+import com.example.enquraandroiddeveloperchallenge.data.BankService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    @Provides
+    @Singleton
+    fun provideBankService(): BankService =
+        Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
+            .build().create(BankService::class.java)
+}
