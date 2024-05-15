75421672697d4245ebe834ac3a7ca1d491bd51fd
Hüseyin Özkoç <huseyin.ozko225@gmail.com>
2024-05-15 15:20:32 +0300

    Added NavHost into the project, and defined screens in NavHost

--- app/src/main/java/com/example/enquraandroiddeveloperchallenge/MainActivity.kt
+++ app/src/main/java/com/example/enquraandroiddeveloperchallenge/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.enquraandroiddeveloperchallenge.ui.theme.EnquraAndroidDeveloperChallengeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +24,26 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EnquraAndroidDeveloperChallengeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.SplashPage.route
+                    ) {
+                        composable(route = Screen.SplashPage.route) {
+                            SplashPage(navController = navController)
+                        }
+                        composable(route = Screen.HomePage.route) {
+                            HomePage(navController = navController)
+                        }
+                        composable(route = Screen.HomePage.route) {
+                            DetailPage(navController = navController)
+                        }
+                    }
                 }
             }
         }
