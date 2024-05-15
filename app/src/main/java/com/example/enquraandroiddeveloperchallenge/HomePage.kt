af49b4dc2ff5be078d1fc4631d1eb92f4c7a0ae1
Hüseyin Özkoç <huseyin.ozko225@gmail.com>
2024-05-15 15:15:15 +0300

    Added Composable to the pages, and added raw animation.

--- app/src/main/java/com/example/enquraandroiddeveloperchallenge/HomePage.kt
+++ app/src/main/java/com/example/enquraandroiddeveloperchallenge/HomePage.kt
@@ -1,2 +1,8 @@
 package com.example.enquraandroiddeveloperchallenge
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HomePage(navController: NavController) {
+}
--- app/src/main/java/com/example/enquraandroiddeveloperchallenge/SplashPage.kt
+++ app/src/main/java/com/example/enquraandroiddeveloperchallenge/SplashPage.kt
@@ -1,2 +1,9 @@
 package com.example.enquraandroiddeveloperchallenge
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun SplashPage(navController: NavController) {
+
+}
